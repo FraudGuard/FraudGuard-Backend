ac654f8b090dd86c5f326a686e81921b20447fd4
Tim Strakeljahn <tim@inncoded.de>
2021-04-29 15:44:49 +0200

    Info

--- src/api/controllers/findById.ts
+++ src/api/controllers/findById.ts
@@ -7,6 +7,7 @@ export const findById = async (req: Request, res: Response) => {
     // TODO is it really a id?? validation, also sanitize input
     const { id } = req.params;
     logger.info(req.params);
+    logger.info('info');
     logger.info(id);
 
     Ads.findById(id)
