bcab4265585b07e47dc8907c67c08bc23594aec2
Jana <jana-rittershofer@web.de>
2021-05-01 13:35:35 +0200

    Schema ergänzen

--- src/services/analyze/analyzeBeschreibung.ts
+++ src/services/analyze/analyzeBeschreibung.ts
@@ -7,6 +7,17 @@ export const analyzeBeschreibung = (
 ) =>
   new Promise((resolve, reject) => {
     logger.info('start analyze Beschreibung');
+
+    var ueberweisung_signalwoerter = ["überweisung", "ueberweisung", "sepa-überweisung", "sepa-ueberwesiung", "überweisen", "ueberweisen","sepa" ];
+
+    var enthaelt_signalwort = 0
+    for ( var signalwort of ueberweisung_signalwoerter){
+        var x = ad['description']?.value?.toLowerCase()
+        if (x.includes(signalwort) ){ enthaelt_signalwort = 1}
+    }
+    resultingAd.beschreibung_enthaelt_ueberweisung = enthaelt_signalwort
+
+
     resultingAd.fraud_score += 0.1;
     if (false) {
       reject(new Error('Some Error happened'));
