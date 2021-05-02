ae4ff84e00710dc71044116ce76635f4b51b61f6
rijana <rijana@users.noreply.github.com>
2021-05-02 13:32:51 +0000

    Prettified Code!

--- src/services/analyze/analyzeBeschreibung.ts
+++ src/services/analyze/analyzeBeschreibung.ts
@@ -1,4 +1,9 @@
-import { AdsFromEbaySchema, AdsSchema, ProductModel , AdsFromEbayModel} from '../../models';
+import {
+  AdsFromEbaySchema,
+  AdsSchema,
+  ProductModel,
+  AdsFromEbayModel,
+} from '../../models';
 import { logger } from '../../shared';
 
 export const analyzeBeschreibung = (
@@ -97,140 +102,194 @@ export const analyzeBeschreibung = (
     resultingAd.beschreibung_enthaelt_neu = enthaelt_signalwort;
 
     //Prüfen auf ovp
-    var ovp_signalwoerter = ["ovp", "original", "originalverpackt", "originalverpackung"];
-    var ovp_signalwoerter_gegenteil = ["nicht ovp", "nicht original", "nicht originalverpackt", "nicht die originalverpackung"];
-    enthaelt_signalwort = 0
-    for ( var signalwort of ovp_signalwoerter){
-        if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 1}
+    var ovp_signalwoerter = [
+      'ovp',
+      'original',
+      'originalverpackt',
+      'originalverpackung',
+    ];
+    var ovp_signalwoerter_gegenteil = [
+      'nicht ovp',
+      'nicht original',
+      'nicht originalverpackt',
+      'nicht die originalverpackung',
+    ];
+    enthaelt_signalwort = 0;
+    for (var signalwort of ovp_signalwoerter) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 1;
+      }
     }
-    for ( var signalwort of ovp_signalwoerter_gegenteil){
-        if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 0}
+    for (var signalwort of ovp_signalwoerter_gegenteil) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 0;
+      }
     }
-    resultingAd.beschreibung_enthaelt_ovp = enthaelt_signalwort
+    resultingAd.beschreibung_enthaelt_ovp = enthaelt_signalwort;
 
-   //Prüfen auf Versiegelung
-   var versiegelt_signalwoerter = ["versiegelt", "verschweißt", "versiegelung", "siegel"];
-   var versiegelt_signalwoerter_gegenteil = ["nicht versiegelt", "nicht verschweißt", "keine versiegelung", "kein siegel"];
-   enthaelt_signalwort = 0
-   for ( var signalwort of versiegelt_signalwoerter){
-       if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 1}
-   }
-   for ( var signalwort of versiegelt_signalwoerter_gegenteil){
-    if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 0}
-}
-   resultingAd.beschreibung_enthaelt_versiegelt = enthaelt_signalwort
+    //Prüfen auf Versiegelung
+    var versiegelt_signalwoerter = [
+      'versiegelt',
+      'verschweißt',
+      'versiegelung',
+      'siegel',
+    ];
+    var versiegelt_signalwoerter_gegenteil = [
+      'nicht versiegelt',
+      'nicht verschweißt',
+      'keine versiegelung',
+      'kein siegel',
+    ];
+    enthaelt_signalwort = 0;
+    for (var signalwort of versiegelt_signalwoerter) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 1;
+      }
+    }
+    for (var signalwort of versiegelt_signalwoerter_gegenteil) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 0;
+      }
+    }
+    resultingAd.beschreibung_enthaelt_versiegelt = enthaelt_signalwort;
 
-   //Prüfen auf Whatsapp
-   var whatsapp_signalwoerter = ["whatsapp", "whats-app", "welche app", "what app"];
-   enthaelt_signalwort = 0
-   for ( var signalwort of whatsapp_signalwoerter){
-       if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 1}
-   }
-   resultingAd.beschreibung_enthaelt_whatsapp = enthaelt_signalwort
+    //Prüfen auf Whatsapp
+    var whatsapp_signalwoerter = [
+      'whatsapp',
+      'whats-app',
+      'welche app',
+      'what app',
+    ];
+    enthaelt_signalwort = 0;
+    for (var signalwort of whatsapp_signalwoerter) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 1;
+      }
+    }
+    resultingAd.beschreibung_enthaelt_whatsapp = enthaelt_signalwort;
 
-  // prüfen auf kopierte Anzeigenbeschreibung
+    // prüfen auf kopierte Anzeigenbeschreibung
 
-  const res = await AdsFromEbayModel.find({
-    'description.value': ad.description.value,
-  });
-  if (res) {
-    resultingAd.beschreibung_ist_kopiert_anzeige = res.length > 1 ? 1 : 0;
-  }
+    const res = await AdsFromEbayModel.find({
+      'description.value': ad.description.value,
+    });
+    if (res) {
+      resultingAd.beschreibung_ist_kopiert_anzeige = res.length > 1 ? 1 : 0;
+    }
 
+    // prüfen auf kopierte Unternehmensbeschreibung
+    enthaelt_signalwort = 0;
+    var produktbeschreibungen = await ProductModel.find(
+      {},
+      { beschreibung: 1 },
+    );
+    for (var produktbeschreibung of produktbeschreibungen) {
+      if (beschreibung.includes(produktbeschreibung.beschreibung)) {
+        enthaelt_signalwort = 1;
+      }
+    }
+    resultingAd.beschreibung_ist_kopiert_unternehmen = enthaelt_signalwort;
 
-  // prüfen auf kopierte Unternehmensbeschreibung
-  enthaelt_signalwort=0
-  var produktbeschreibungen =  await ProductModel.find({},{"beschreibung":1});
-  for(var produktbeschreibung of produktbeschreibungen){
-    if (beschreibung.includes(produktbeschreibung.beschreibung)) {enthaelt_signalwort =1}
-  }
-  resultingAd.beschreibung_ist_kopiert_unternehmen = enthaelt_signalwort
+    //Antipattern
+    //Prüfen auf Barzahlung
+    var barzahlung_signalwoerter = ['barzahlung', 'bar', 'bezahlung vor ort'];
+    var barzahlung_signalwoerter_gegenteil = [
+      'keine barzahlung',
+      'kein bar',
+      'keine bezahlung vor ort',
+    ];
+    enthaelt_signalwort = 0;
+    for (var signalwort of barzahlung_signalwoerter) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 1;
+      }
+    }
+    for (var signalwort of barzahlung_signalwoerter_gegenteil) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 0;
+      }
+    }
+    resultingAd.ap_beschreibung_enthaelt_barzahlung = enthaelt_signalwort;
 
+    //Prüfen, ob Artikel gebraucht ist
+    var gebraucht_signalwoerter = [
+      'gebrauchsspuren',
+      'benutzt',
+      'gebraucht',
+      'aufgebaut',
+      'zusammengebaut',
+    ];
+    var gebraucht_signalwoerter_gegenteil = [
+      'keine gebrauchsspuren',
+      'nicht benutzt',
+      'nicht gebraucht',
+      'nie aufgebaut',
+      'nie zusammengebaut',
+    ];
+    enthaelt_signalwort = 0;
+    for (var signalwort of gebraucht_signalwoerter) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 1;
+      }
+    }
+    for (var signalwort of gebraucht_signalwoerter_gegenteil) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 0;
+      }
+    }
+    resultingAd.ap_beschreibung_enthaelt_gebraucht = enthaelt_signalwort;
 
+    //Prüfen auf Tausch
+    var tausch_signalwoerter = [
+      'tausch',
+      'tausche',
+      'tauschangebot',
+      'tauschen',
+    ];
+    var tausch_signalwoerter_gegenteil = [
+      'kein tausch',
+      'tausche nicht',
+      'kein tauschangebot',
+      'nicht tauschen',
+    ];
+    enthaelt_signalwort = 0;
+    for (var signalwort of tausch_signalwoerter) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 1;
+      }
+    }
+    for (var signalwort of tausch_signalwoerter_gegenteil) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 0;
+      }
+    }
+    resultingAd.ap_beschreibung_enthaelt_tausch = enthaelt_signalwort;
 
-  //Antipattern
-  //Prüfen auf Barzahlung
-  var barzahlung_signalwoerter = ["barzahlung", "bar", "bezahlung vor ort"];
-  var barzahlung_signalwoerter_gegenteil = ["keine barzahlung", "kein bar", "keine bezahlung vor ort"];
-  enthaelt_signalwort = 0
-  for ( var signalwort of barzahlung_signalwoerter){
-      if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 1}
-  }
-  for ( var signalwort of barzahlung_signalwoerter_gegenteil){
-    if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 0}
-  }
-  resultingAd.ap_beschreibung_enthaelt_barzahlung = enthaelt_signalwort
-
-  //Prüfen, ob Artikel gebraucht ist
-  var gebraucht_signalwoerter = ["gebrauchsspuren", "benutzt", "gebraucht", "aufgebaut", "zusammengebaut"];
-  var gebraucht_signalwoerter_gegenteil = ["keine gebrauchsspuren","nicht benutzt", "nicht gebraucht",  "nie aufgebaut", "nie zusammengebaut"];
-  enthaelt_signalwort = 0
-  for ( var signalwort of gebraucht_signalwoerter){
-      if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 1}
-  }
-  for ( var signalwort of gebraucht_signalwoerter_gegenteil){
-    if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 0}
-}
-  resultingAd.ap_beschreibung_enthaelt_gebraucht = enthaelt_signalwort
-
-
-  //Prüfen auf Tausch
-  var tausch_signalwoerter = ["tausch", "tausche", "tauschangebot", "tauschen"];
-  var tausch_signalwoerter_gegenteil = ["kein tausch", "tausche nicht", "kein tauschangebot", "nicht tauschen"];
-  enthaelt_signalwort = 0
-  for ( var signalwort of tausch_signalwoerter){
-      if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 1}
-  }
-  for ( var signalwort of tausch_signalwoerter_gegenteil){
-    if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 0}
-}
-  resultingAd.ap_beschreibung_enthaelt_tausch = enthaelt_signalwort
-
-  //Prüfen auf Abholung
-  var abholung_signalwoerter = ["abholung", "nur abholung", "selbstabholer", "abholen"];
-  var abholung_signalwoerter_gegenteil = ["keine abholung", "keine abholung", "kein selbstabholer", "nicht abholen"];
-  enthaelt_signalwort = 0
-  for ( var signalwort of abholung_signalwoerter){
-      if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 1}
-  }
-  for ( var signalwort of abholung_signalwoerter_gegenteil){
-    if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 0}
-}
-  resultingAd.ap_beschreibung_enthaelt_abholung = enthaelt_signalwort
-  
-  //Prüfen auf Suche
-  var suche_signalwoerter = ["suche", "gesucht"];
-  var suche_signalwoerter_gegenteil = ["keine suche", "nicht gesucht"];
-  enthaelt_signalwort = 0
-  for ( var signalwort of suche_signalwoerter){
-      if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 1}
-  }
-  for ( var signalwort of suche_signalwoerter_gegenteil){
-    if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 0}
-}
-  resultingAd.ap_beschreibung_enthaelt_suche = enthaelt_signalwort
-
-  //Prüfen auf Sammlungsauflösung
-  var sammleraufloesung_signalwoerter = ["sammlerauflösung", "sammlungsauflösung", "sammlung", "löse sammlung auf", "löse meine sammlung auf"];
-  var sammleraufloesung_signalwoerter_gegenteil = ["keine sammlerauflösung", "keine sammlungsauflösung", "keine sammlung", "löse sammlung nicht auf", "löse meine sammlung nicht auf"];
-  enthaelt_signalwort = 0
-  for ( var signalwort of sammleraufloesung_signalwoerter){
-      if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 1}
-  }
-  for ( var signalwort of sammleraufloesung_signalwoerter_gegenteil){
-    if (beschreibung.includes(signalwort)){ enthaelt_signalwort = 0}
-}
-  resultingAd.ap_beschreibung_enthaelt_sammleraufloesung = enthaelt_signalwort
-
-
-  //Prüfen auf Verkauf mit Kiloangabe
-  var kilo_signalwoerter = ["kilo", " kg ", "kilogramm"];
-
-  enthaelt_signalwort = 0
-  for ( var signalwort of kilo_signalwoerter){
-      if (beschreibung.includes(signalwort) ){ enthaelt_signalwort = 1}
-  }
-  resultingAd.ap_beschreibung_enthaelt_kilo = enthaelt_signalwort
+    //Prüfen auf Abholung
+    var abholung_signalwoerter = [
+      'abholung',
+      'nur abholung',
+      'selbstabholer',
+      'abholen',
+    ];
+    var abholung_signalwoerter_gegenteil = [
+      'keine abholung',
+      'keine abholung',
+      'kein selbstabholer',
+      'nicht abholen',
+    ];
+    enthaelt_signalwort = 0;
+    for (var signalwort of abholung_signalwoerter) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 1;
+      }
+    }
+    for (var signalwort of abholung_signalwoerter_gegenteil) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 0;
+      }
+    }
+    resultingAd.ap_beschreibung_enthaelt_abholung = enthaelt_signalwort;
 
     //Prüfen auf Suche
     var suche_signalwoerter = ['suche', 'gesucht'];
@@ -248,6 +307,60 @@ export const analyzeBeschreibung = (
     }
     resultingAd.ap_beschreibung_enthaelt_suche = enthaelt_signalwort;
 
+    //Prüfen auf Sammlungsauflösung
+    var sammleraufloesung_signalwoerter = [
+      'sammlerauflösung',
+      'sammlungsauflösung',
+      'sammlung',
+      'löse sammlung auf',
+      'löse meine sammlung auf',
+    ];
+    var sammleraufloesung_signalwoerter_gegenteil = [
+      'keine sammlerauflösung',
+      'keine sammlungsauflösung',
+      'keine sammlung',
+      'löse sammlung nicht auf',
+      'löse meine sammlung nicht auf',
+    ];
+    enthaelt_signalwort = 0;
+    for (var signalwort of sammleraufloesung_signalwoerter) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 1;
+      }
+    }
+    for (var signalwort of sammleraufloesung_signalwoerter_gegenteil) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 0;
+      }
+    }
+    resultingAd.ap_beschreibung_enthaelt_sammleraufloesung = enthaelt_signalwort;
+
+    //Prüfen auf Verkauf mit Kiloangabe
+    var kilo_signalwoerter = ['kilo', ' kg ', 'kilogramm'];
+
+    enthaelt_signalwort = 0;
+    for (var signalwort of kilo_signalwoerter) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 1;
+      }
+    }
+    resultingAd.ap_beschreibung_enthaelt_kilo = enthaelt_signalwort;
+
+    //Prüfen auf Suche
+    var suche_signalwoerter = ['suche', 'gesucht'];
+    var suche_signalwoerter_gegenteil = ['keine suche', 'nicht gesucht'];
+    enthaelt_signalwort = 0;
+    for (var signalwort of suche_signalwoerter) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 1;
+      }
+    }
+    for (var signalwort of suche_signalwoerter_gegenteil) {
+      if (beschreibung.includes(signalwort)) {
+        enthaelt_signalwort = 0;
+      }
+    }
+    resultingAd.ap_beschreibung_enthaelt_suche = enthaelt_signalwort;
 
     //Prüfen auf Verkauf mit Kiloangabe
     var kilo_signalwoerter = ['kilo', ' kg ', 'kilogramm'];
