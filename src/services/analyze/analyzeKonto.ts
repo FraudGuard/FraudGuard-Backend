edd203f6098b0e624e07b0a7604eabcd88651b3b
Tim Strakeljahn <tim@inncoded.de>
2021-05-06 23:14:08 +0200

    analyzeKonto

--- src/services/analyze/analyzeKonto.ts
+++ src/services/analyze/analyzeKonto.ts
@@ -1,11 +1,15 @@
 import { AdsFromEbaySchema, AdsSchema } from '../../api/models';
 import { logger } from '../../shared';
+import { getAllByAccount } from '../ebay/getAllByAccount';
+import firstNames from '../../assets/data/firstNames.json'
+import countries from '../../assets/data/world.json'
 
 export const analyzeKonto = async (
   ad: AdsFromEbaySchema,
   resultingAd: AdsSchema,
 ) => {
   logger.info('analyze Konto');
+  const adsFromAccount = await getAllByAccount(ad['user-id'].value.toString())
 
   const badgesMap: any = {};
   ad.userBadges?.[0]?.badges?.forEach((x) => (badgesMap[x.name] = x));
@@ -44,16 +48,30 @@ export const analyzeKonto = async (
   // } else {
   //   resultingAd.konto_privat = 0
   // }
+  const foundSpecialChars = ad['contact-name'].value.match(/[@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g);
+  resultingAd.konto_name_sonderzeichen_anzahl = foundSpecialChars?.length ?? 0
+
+  resultingAd.konto_erstellt_timestamp = new Date(ad['user-since-date-time'].value).getTime()
+
+  resultingAd.konto_name_natuerlich = firstNames.find(x => ad['contact-name'].value.toLocaleLowerCase().includes(x.toLocaleLowerCase())) ? 1 : 0
+  resultingAd.konto_name_enthaelt_unueblich = countries.find(x => ad['contact-name'].value.toLocaleLowerCase().includes(x.name.toLocaleLowerCase())) ? 1 : 0
+
+
+  if (adsFromAccount) {
+    resultingAd.konto_anzeigen_anzahl = adsFromAccount.length
+    resultingAd.konto_anzeigen_ueber_100 = adsFromAccount.filter(x => x.price.amount.value > 100).length
+
+    resultingAd.konto_anzeigen_gleich = 0;
+    for (const adFromAccount of adsFromAccount) {
+      if (
+        adFromAccount.title.value.includes(ad.title.value.toLocaleLowerCase())
+        && adFromAccount.description.value.includes(ad.description.value.toLocaleLowerCase())
+      ) {
+        ++resultingAd.konto_anzeigen_gleich
+      }
+    }
+  }
 
   // Todo
-  // resultingAd.konto_erstellt_timestamp = ad.
-
-  // resultingAd.konto_anzeigen_anzahl = ad.
   // resultingAd.konto_anzeigen_betrugsrate = ad.
-  // resultingAd.konto_anzeigen_ueber_100 = ad.
-  //   resultingAd.konto_anzeigen_gleich = ad.
-
-  // resultingAd.konto_name_sonderzeichen_anzahl = ad.
-  // resultingAd.konto_name_enthaelt_unueblich = ad.
-  // resultingAd.konto_name_natuerlich = ad.
 };
---
+++ src/services/ebay/getAllByAccount.ts
@@ -0,0 +1,25 @@
+import { AdsFromEbayModel, AdsFromEbaySchema } from '../../api/models';
+import { ebayHeaders } from './ebay';
+import { logger } from '../../shared';
+import axios, { AxiosRequestConfig } from 'axios';
+
+export const getAllByAccount = (accountId: string): Promise<AdsFromEbaySchema[]> =>
+  new Promise((resolve, reject) => {
+    const config: AxiosRequestConfig = {
+      method: 'get',
+      url: `https://api.ebay-kleinanzeigen.de/api/ads.json?histograms=CATEGORY&limitTotalResultCount=true&locationId=0&userIds=${accountId}&size=200`,
+      headers: ebayHeaders,
+    };
+
+    axios(config)
+      .then((response: any) => {
+        resolve(
+          response.data['{http://www.ebayclassifiedsgroup.com/schema/ad/v1}ads']
+            .value.ad?.map((x: any) => new AdsFromEbayModel(x))
+        );
+      })
+      .catch((error: any) => {
+        logger.error(error.body);
+        reject(error);
+      });
+  });
