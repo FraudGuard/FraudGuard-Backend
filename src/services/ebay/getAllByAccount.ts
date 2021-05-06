fe134e9737d1c0757aa583cbb325c3e88df3ad4d
timstrakeljahn <timstrakeljahn@users.noreply.github.com>
2021-05-06 21:14:48 +0000

    Prettified Code!

--- src/services/ebay/getAllByAccount.ts
+++ src/services/ebay/getAllByAccount.ts
@@ -3,7 +3,9 @@ import { ebayHeaders } from './ebay';
 import { logger } from '../../shared';
 import axios, { AxiosRequestConfig } from 'axios';
 
-export const getAllByAccount = (accountId: string): Promise<AdsFromEbaySchema[]> =>
+export const getAllByAccount = (
+  accountId: string,
+): Promise<AdsFromEbaySchema[]> =>
   new Promise((resolve, reject) => {
     const config: AxiosRequestConfig = {
       method: 'get',
@@ -14,8 +16,9 @@ export const getAllByAccount = (accountId: string): Promise<AdsFromEbaySchema[]>
     axios(config)
       .then((response: any) => {
         resolve(
-          response.data['{http://www.ebayclassifiedsgroup.com/schema/ad/v1}ads']
-            .value.ad?.map((x: any) => new AdsFromEbayModel(x))
+          response.data[
+            '{http://www.ebayclassifiedsgroup.com/schema/ad/v1}ads'
+          ].value.ad?.map((x: any) => new AdsFromEbayModel(x)),
         );
       })
       .catch((error: any) => {
