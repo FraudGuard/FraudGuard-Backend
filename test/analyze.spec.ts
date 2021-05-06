bded331795693f2741e47a5a4a7decd8da599085
Michelle <semi1016@h-ka.de>
2021-05-06 14:33:42 +0200

    testTitel

--- test/analyze.spec.ts
+++ test/analyze.spec.ts
@@ -28,37 +28,70 @@ describe('Analyze Ads', () => {
   });
 
   test('Analyze titel', async () => {
+    // keine Merkmale enthalten
     const result1 = await analyzeTitel(Ad1, new AdsModel());
-    const result2 = await analyzeTitel(Ad2, new AdsModel());
 
-    // Titel enthählt neu
+    // Titel enthält nicht neu
     expect(result1.titel_enthaelt_neu).to.be.equal(0);
 
-    // Titel enthält ovp
+    // Titel enthält nicht ovp
     expect(result1.titel_enthaelt_ovp).to.be.equal(0);
 
-    // Titel enthält verschweißt
+    // Titel enthält nicht verschweißt
     expect(result1.titel_enthaelt_verschweißt).to.be.equal(0);
 
-    // Titel enthält ungeöffnet
+    // Titel enthält nicht ungeöffnet
     expect(result1.titel_enthaelt_ungeoeffnet).to.be.equal(0);
 
-    // Titel enthält Zeichen
+    // Titel enthält kein Zeichen
     expect(result1.titel_enthaelt_zeichen).to.be.equal(0);
 
-    // Anitpattern: Titel enthält gebraucht
+    // Anitpattern: Titel enthält nicht gebraucht
     expect(result1.ap_titel_enthaelt_gebraucht).to.be.equal(0);
 
-    // Antipattern: Titel enthält suche
+    // Antipattern: Titel enthält keine suche
     expect(result1.ap_titel_enthaelt_suche).to.be.equal(0);
 
-    // Antipattern: Titel enthält tausche
+    // Antipattern: Titel enthält nicht tausche
     expect(result1.ap_titel_enthaelt_tausche).to.be.equal(0);
 
-    // Antipattern: Titel enthält Sammlung
+    // Antipattern: Titel enthält keine Sammlung
     expect(result1.ap_titel_enthaelt_sammlung).to.be.equal(0);
 
-    // Antipattern: Titel enthält Kilo
+    // Antipattern: Titel enthält nicht Kilo
+    expect(result1.ap_titel_enthaelt_kilo).to.be.equal(0);
+
+    // Betrugsmerkmale enthalten
+    const result2 = await analyzeTitel(Ad2, new AdsModel());
+
+    // Titel enthält neu
+    expect(result2.titel_enthaelt_neu).to.be.equal(1);
+
+    // Titel enthält ovp
+    expect (result2.titel_enthaelt_ovp).to.be.equal(1);
+
+    // Titel enthält verschweißt
+    expect(result2.titel_enthaelt_verschweißt).to.be.equal(1);
+
+    // Titel enthält ungeöffnet
+    expect(result2.titel_enthaelt_ungeoeffnet).to.be.equal(1);
+
+    // Titel enthält ein Zeichen
+    expect(result2.titel_enthaelt_zeichen).to.be.equal(1);
+
+    // Anitpattern: Titel enthält nicht gebraucht
+    expect(result1.ap_titel_enthaelt_gebraucht).to.be.equal(0);
+
+    // Antipattern: Titel enthält keine suche
+    expect(result1.ap_titel_enthaelt_suche).to.be.equal(0);
+    
+    // Antipattern: Titel enthält nicht tausche
+    expect(result1.ap_titel_enthaelt_tausche).to.be.equal(0);
+    
+    // Antipattern: Titel enthält keine Sammlung
+    expect(result1.ap_titel_enthaelt_sammlung).to.be.equal(0);
+    
+    // Antipattern: Titel enthält nicht Kilo
     expect(result1.ap_titel_enthaelt_kilo).to.be.equal(0);
   });
 
