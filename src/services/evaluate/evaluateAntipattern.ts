75ab0049e1258a5e96cd604992df6441e75f9062
timstrakeljahn <timstrakeljahn@users.noreply.github.com>
2021-05-15 09:38:28 +0000

    Prettified Code!

--- src/services/evaluate/evaluateAntipattern.ts
+++ src/services/evaluate/evaluateAntipattern.ts
@@ -1,48 +1,47 @@
-import { AdsSchema } from "../../api/models";
-import { logger } from "../../shared";
+import { AdsSchema } from '../../api/models';
+import { logger } from '../../shared';
 
-export const evaluateAntipattern = async (
-    resultingAd: AdsSchema
-) => {
-    logger.info('evaluate Antipattern');
+export const evaluateAntipattern = async (resultingAd: AdsSchema) => {
+  logger.info('evaluate Antipattern');
 
-    resultingAd.antipattern_score = 0
-    resultingAd.antipattern_gesamtscore = 0
-    resultingAd.antipattern_anzahl_zutreffend = 0
-    resultingAd.antipattern_anzahl_gesamt = 0
+  resultingAd.antipattern_score = 0;
+  resultingAd.antipattern_gesamtscore = 0;
+  resultingAd.antipattern_anzahl_zutreffend = 0;
+  resultingAd.antipattern_anzahl_gesamt = 0;
 
-    // BEISPIEL
+  // BEISPIEL
 
-    // addToScore(resultingAd immer eingeben, bedingung wann addiert werden soll, score)
-    addToScore(resultingAd, resultingAd.ap_beschreibung_enthaelt_barzahlung === 1, 5)
+  // addToScore(resultingAd immer eingeben, bedingung wann addiert werden soll, score)
+  addToScore(
+    resultingAd,
+    resultingAd.ap_beschreibung_enthaelt_barzahlung === 1,
+    5,
+  );
 
+  // BEISPIEL ENDE
 
-    // BEISPIEL ENDE
+  // Michelle
 
+  // Jana
 
-    // Michelle
+  // Jessi
 
+  return resultingAd;
+};
 
-    // Jana
+const addToScore = (
+  resultingAd: AdsSchema,
+  condition: boolean,
+  score: number,
+): void => {
+  if (score < 1 || score > 5) {
+    throw new Error('Score not in Range');
+  }
 
-
-    // Jessi
-
-
-    return resultingAd;
-}
-
-
-const addToScore = (resultingAd: AdsSchema, condition: boolean, score: number): void => {
-    if (score < 1 || score > 5) {
-        throw new Error('Score not in Range')
-    }
-
-    if (condition) {
-        resultingAd.antipattern_score += score
-        resultingAd.antipattern_anzahl_zutreffend += 1
-
-    }
-    resultingAd.antipattern_gesamtscore += score
-    resultingAd.antipattern_anzahl_gesamt += 1
-}
+  if (condition) {
+    resultingAd.antipattern_score += score;
+    resultingAd.antipattern_anzahl_zutreffend += 1;
+  }
+  resultingAd.antipattern_gesamtscore += score;
+  resultingAd.antipattern_anzahl_gesamt += 1;
+};
