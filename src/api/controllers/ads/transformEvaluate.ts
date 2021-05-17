dcbe39f46b3758b945a68b91ca2a7ccdd4ea4bdf
Tim Strakeljahn <tim@inncoded.de>
2021-05-17 14:35:55 +0200

    transformEvaluate

---
+++ src/api/controllers/ads/transformEvaluate.ts
@@ -0,0 +1,44 @@
+import { HttpStatus, logger } from '../../../shared';
+import { Request, Response } from 'express';
+import { AdsModel, AdsSchema } from '../../models';
+import { evaluate } from '../../../services/evaluate';
+
+const skipValue = 1000
+export const transformEvaluate = async (_req: Request, res: Response) => {
+  try {
+    logger.info('transformEvaluate');
+    runEvaluate(0);
+    res.status(HttpStatus.OK).json({ result: true });
+  } catch (err) {
+    res.status(HttpStatus.INTERNAL_ERROR).json({ error: err });
+    logger.error(err);
+  }
+};
+
+const runEvaluate = async (skip = 0) => {
+  const items = await AdsModel.find({}).skip(skip).limit(skipValue);
+  console.log('items', items.length);
+
+  const promises: Promise<any>[] = [];
+  items.forEach(async (ad, i) => {
+    // for (const ad of items) {
+    promises.push(singleEvaluate(ad, i));
+  });
+  const data = await Promise.all(promises);
+  await AdsModel.bulkWrite(data.filter(x => x))
+  logger.info('updated')
+  if (items.length === skipValue) {
+    runEvaluate(skip + skipValue);
+  }
+};
+
+const singleEvaluate = (ad: AdsSchema, i: number) =>
+  new Promise(async (resolve, reject) => {
+    // const result = await analyze(ad);
+    const result = await evaluate(ad).catch((error) => reject(error));
+    if (!result) {
+      return 
+    }
+    console.log('analyzed', ++i);
+    resolve({ updateOne: { filter: { _id: result._id }, update: result.toObject(), upsert: true }});
+  });
--- src/api/routes/adsRouter.ts
+++ src/api/routes/adsRouter.ts
@@ -1,11 +1,13 @@
 import { findById } from '../controllers';
 import express, { Router } from 'express';
 import { transform } from '../controllers/ads/transform';
+import { transformEvaluate } from '../controllers/ads/transformEvaluate';
 
 // eslint-disable-next-line new-cap
 const adsRouter: Router = express.Router();
 
 // TODO SWAGGER.io doc
+adsRouter.route('/transform/evaluate').get(transformEvaluate);
 adsRouter.route('/transform').get(transform);
 adsRouter.route('/:id').get(findById);
 
