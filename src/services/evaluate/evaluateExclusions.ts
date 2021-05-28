b2acdff456a7987aa259d33464ca8452f2de8a7c
rijana <rijana@users.noreply.github.com>
2021-05-28 12:12:02 +0000

    Prettified Code!

--- src/services/evaluate/evaluateExclusions.ts
+++ src/services/evaluate/evaluateExclusions.ts
@@ -8,37 +8,47 @@ import { AdsSchema } from '../../api/models';
  * @return {number} Gibt zurück, ob eine Auschlusskriterium erfüllt ist
  */
 export const evaluateExclusions = async (resultingAd: AdsSchema) => {
+  let ausschlusskriterium_erfuellt = 0;
 
-    let ausschlusskriterium_erfuellt = 0;
+  if (resultingAd.ap_titel_enthaelt_gebraucht === 1) {
+    ausschlusskriterium_erfuellt = 1;
+  }
+  if (resultingAd.ap_titel_enthaelt_suche === 1) {
+    ausschlusskriterium_erfuellt = 1;
+  }
+  if (resultingAd.ap_titel_enthaelt_tausche === 1) {
+    ausschlusskriterium_erfuellt = 1;
+  }
+  if (resultingAd.ap_titel_enthaelt_kilo === 1) {
+    ausschlusskriterium_erfuellt = 1;
+  }
+  if (resultingAd.ap_titel_enthaelt_sammlung === 1) {
+    ausschlusskriterium_erfuellt = 1;
+  }
 
-    if (resultingAd.ap_titel_enthaelt_gebraucht === 1) { ausschlusskriterium_erfuellt = 1; }
-    if (resultingAd.ap_titel_enthaelt_suche === 1) { ausschlusskriterium_erfuellt = 1; }
-    if (resultingAd.ap_titel_enthaelt_tausche === 1) { ausschlusskriterium_erfuellt = 1; }
-    if (resultingAd.ap_titel_enthaelt_kilo === 1) { ausschlusskriterium_erfuellt = 1; }
-    if (resultingAd.ap_titel_enthaelt_sammlung === 1) { ausschlusskriterium_erfuellt = 1; }
+  if (resultingAd.ap_beschreibung_enthaelt_tausch === 1) {
+    ausschlusskriterium_erfuellt = 1;
+  }
+  if (resultingAd.ap_beschreibung_enthaelt_suche === 1) {
+    ausschlusskriterium_erfuellt = 1;
+  }
 
+  if (resultingAd.ap_preis_ist_leer === 1) {
+    ausschlusskriterium_erfuellt = 1;
+  }
 
-    if (resultingAd.ap_beschreibung_enthaelt_tausch === 1) { ausschlusskriterium_erfuellt = 1; }
-    if (resultingAd.ap_beschreibung_enthaelt_suche === 1) { ausschlusskriterium_erfuellt = 1; }
+  if (resultingAd.ap_sonstiges_anzeige_suche === 1) {
+    ausschlusskriterium_erfuellt = 1;
+  }
 
+  if (
+    resultingAd.ap_sonstiges_anzeige_nur_abholung === 1 &&
+    resultingAd.ap_beschreibung_enthaelt_barzahlung === 1 &&
+    resultingAd.beschreibung_enthaelt_ueberweisung === 0 &&
+    resultingAd.beschreibung_enthaelt_sepa === 0
+  ) {
+    ausschlusskriterium_erfuellt = 1;
+  }
 
-    if (resultingAd.ap_preis_ist_leer === 1) { ausschlusskriterium_erfuellt = 1; }
-
-
-    if (resultingAd.ap_sonstiges_anzeige_suche === 1) { ausschlusskriterium_erfuellt = 1; }
-
-
-    if (
-        resultingAd.ap_sonstiges_anzeige_nur_abholung === 1 &&
-        resultingAd.ap_beschreibung_enthaelt_barzahlung === 1 &&
-        resultingAd.beschreibung_enthaelt_ueberweisung === 0 &&
-        resultingAd.beschreibung_enthaelt_sepa === 0
-    ) {
-        ausschlusskriterium_erfuellt = 1;
-    }
-
-
-    return ausschlusskriterium_erfuellt;
+  return ausschlusskriterium_erfuellt;
 };
-
-
